6554f5f87f3cd15698693c9e4294d20c405eff61
aminnsabet <aminnsabet@gmail.com>
2024-06-07 11:28:34 +0000

    update the ports in config file

--- API/test.py
+++ API/test.py
@@ -1,14 +1,5 @@
-from langchain_community.llms import HuggingFaceTextGenInference
-from langchain.chains import LLMChain
-from app.routers.LLM.backend_database import Database
-import json
-my_data = Database()
+import transformers
 
-my_data.update_conversation( {
-                    "username": "admin",
-                    "content": "HI",
-                    "gen_token_number": 1,
-                    "prompt_token_number": 2,
-                    "conversation_number": 3,
-                })
-
+transformers.AutoTokenizer.from_pretrained(
+            "meta-llama/Llama-2-7b-chat-hf", padding_side="left", token ="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"
+        )
