63f0a12d586697fed194b785466c3fbd81ef4f9e
AminSabet <39775559+aminnsabet@users.noreply.github.com>
2024-06-19 15:48:10 +0100

    Update 1_chatbot.py

--- UI/pages/1_chatbot.py
+++ UI/pages/1_chatbot.py
@@ -1,75 +1,33 @@
 import streamlit as st
 import requests
-
-from langchain.document_loaders import YoutubeLoader
-import os
-import streamlit as st
-import requests
 import json
 import time
 from langchain.schema import messages_from_dict, messages_to_dict
 from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
-import random
 from langchain_community.chat_message_histories import SQLChatMessageHistory
 
 BASE_URL = "http://localhost:8086"
 Weaviate_endpoint = "/vector_DB_request/"
 
 def process_text(text):
-    # Remove quotes from the beginning and end of the text, if present
     if text.startswith('"') and text.endswith('"'):
         text = text[1:-1]
-
-    # Replace \n with an actual new line
     text = text.replace("\\n", "\n")
-
     return text
 
-def display_user_classes(username, access_token):
-    params = {
-        "username": username,
-        "mode": "display_classes",
-        "vectorDB_type": "Weaviate",
-        "mode": "display_classes",
-        "class_name": "string"
-        }
-    file_path = None
-
-    #headers = {"Authorization": f"Bearer {access_token}"}
-    resp = send_vector_db_request(access_token, params, Weaviate_endpoint)
-    #resp = requests.post(f"{BASE_URL}/vector_DB_request/",json=params, headers=headers)
-        # Handle the response
-    print("the response", resp, resp.content)
-
-    response_content = resp.content.decode("utf-8")
-    print("respcontent", response_content)
-    user_classes = json.loads(response_content)
-    if resp.status_code == 200:
-        print(resp.status_code, resp.content)
-        return user_classes
-    else:
-        print(resp.status_code, resp.content)
-        return 
-    
 def send_vector_db_request(access_token, json_data, endpoint, uploaded_file=None):
     headers = {"Authorization": f"Bearer {access_token}"}
-
-
-    response = requests.post(f"{BASE_URL}{endpoint}", data=json_data,headers=headers, files=uploaded_file)
-
+    response = requests.post(f"{BASE_URL}{endpoint}", data=json_data, headers=headers, files=uploaded_file)
     return response    
 
 def authentication(username, password):
     data = {"username": username, "password": password}
-    resp = requests.post(
-        f"{BASE_URL}/token", data=data
-    )
+    resp = requests.post(f"{BASE_URL}/token", data=data)
     if "access_token" not in resp.json():
         return None
     return resp.json()["access_token"]
 
-def add_user(username, password,gen_token_limit,prompt_token_limit,access_token):
-
+def add_user(username, password, gen_token_limit, prompt_token_limit, access_token):
     headers = {"Authorization": f"Bearer {access_token}"}
     query_data = {
         "username": username,
@@ -78,7 +36,7 @@ def add_user(username, password,gen_token_limit,prompt_token_limit,access_token)
         "gen_token_number": int(0),
         "gen_token_limit": int(gen_token_limit),
         "prompt_token_limit": int(prompt_token_limit)
-        }
+    }
     resp = requests.post(f"{BASE_URL}/db_request/add_user/", json=query_data, headers=headers)
     return True
 
@@ -91,11 +49,9 @@ def get_all_users_info(access_token):
         return None
 
 def retrieve_latest_conversation(username, access_token):
-    query_data = {
-  "username": username
-}
+    query_data = {"username": username}
     headers = {"Authorization": f"Bearer {access_token}"}
-    resp = requests.post(f"{BASE_URL}/db_request/get_user_conversations/",json=query_data, headers=headers)
+    resp = requests.post(f"{BASE_URL}/db_request/get_user_conversations/", json=query_data, headers=headers)
     if resp.status_code == 200:
         conversations = resp.json()['conversations']
         names = [d["name"] for d in conversations]
@@ -105,74 +61,49 @@ def retrieve_latest_conversation(username, access_token):
     else:
         return None
     
-def add_conversation(username, conversation,access_token):
+def add_conversation(username, conversation, access_token):
     query_data = {
-  "username": username,
-  "content": json.dumps(conversation),
-  "conversation_name": "Current Conversation",
-}
+        "username": username,
+        "content": json.dumps(conversation),
+        "conversation_name": "Current Conversation",
+    }
     headers = {"Authorization": f"Bearer {access_token}"}
-    resp = requests.post(f"{BASE_URL}/db_request/add_conversation/",json=query_data, headers=headers)
+    resp = requests.post(f"{BASE_URL}/db_request/add_conversation/", json=query_data, headers=headers)
     if resp.status_code == 200:
         return True
     else:
         return False
 
-def chat(model, inference_endpoint,prompt, memory, username, conversation_number,access_token):
+def chat(model, inference_endpoint, prompt, memory, username, conversation_number, access_token):
     headers = {"Authorization": f"Bearer {access_token}"}
-    quesry_data = {
+    query_data = {
         "model": model,
-        "inference_endpoint":  inference_endpoint   ,
+        "inference_endpoint": inference_endpoint,
         "prompt": prompt,
         "memory": memory,
         "conversation_number": conversation_number,
         "username": username
-        }
-    resp = requests.post(f"{BASE_URL}/llm_request",json=quesry_data, headers=headers)
+    }
+    resp = requests.post(f"{BASE_URL}/llm_request", json=query_data, headers=headers)
     return resp.json()
 
-def retrieving_messages(username,access_token, converation_number=None):
-
+def retrieving_messages(username, access_token, conversation_number=None):
     headers = {"Authorization": f"Bearer {access_token}"}
-    if converation_number:  
-        query_data = {
+    query_data = {
         "username": username,
-        "conversation_number": converation_number
-        }
-    else:
-        query_data = {
-            "username": username
-            }
-    resp = requests.post(f"{BASE_URL}/db_request/retrieve_conversation",json=query_data, headers=headers)
+        "conversation_number": conversation_number
+    }
+    resp = requests.post(f"{BASE_URL}/db_request/retrieve_conversation", json=query_data, headers=headers)
     
     if resp.status_code == 200:
         if resp.json()["conversation_id"]:
-            messages = SQLChatMessageHistory(resp.json()["conversation_id"],"sqlite:////home/amin_sabet/dev/LLM-Chat-Bot/API/memory.db").get_messages()
+            messages = SQLChatMessageHistory(resp.json()["conversation_id"], "sqlite:////home/amin_sabet/dev/LLM-Chat-Bot/API/memory.db").get_messages()
             return messages
         else:
             return None
     else:
         return None
 
-    
-
-def add_conversation(username, conversation_name,access_token):
-    if not conversation_name:
-        query_data = {
-                        "username": username,
-                        "conversation_name": conversation_name
-                        }
-    else:
-        query_data = {
-                        "username": username,
-                        "conversation_name": conversation_name
-                        }
-    headers = {"Authorization": f"Bearer {access_token}"}
-    resp = requests.post(f"{BASE_URL}/db_request/add_conversation/",json=query_data, headers=headers)
-    if resp.status_code == 200:
-        return resp.json()
-    else:   
-        return None
 def add_LLM(model_name, access_token, HF_ACCESS_TOKEN, MAX_MODEL_LEN, SEED):
     headers = {"Authorization": f"Bearer {access_token}"}
     query_data = {
@@ -184,45 +115,46 @@ def add_LLM(model_name, access_token, HF_ACCESS_TOKEN, MAX_MODEL_LEN, SEED):
         "SEED": int(SEED),
         "QUANTIZATION": "None"
     }
-
     resp = requests.post(f"{BASE_URL}/vllm_init", json=query_data, headers=headers)
 
     if resp.status_code == 200:
         if resp.json()["status"] == "healthy":
             return resp.json()
         else:
-            return false
+            return False
     else:
         return False
 
+def retrieve_user_conversations_info(username, access_token):
+    query_data = {"username": username}
+    headers = {"Authorization": f"Bearer {access_token}"}
+    resp = requests.post(f"{BASE_URL}/db_request/retrieve_all_conversations/", json=query_data, headers=headers)
+    if resp.status_code == 200:
+        return resp.json()
+
 if "username" not in st.session_state or st.sidebar.button("Logout"):
-    # Login form
     if "username" not in st.session_state:
         username = st.text_input("Enter your username:")
-        password = st.text_input('Enter your password', type='password') 
+        password = st.text_input('Enter your password', type='password')
     else:
         username = st.session_state.username
 
-    if  st.button("Login"):  # Add password field
-                token =  authentication(username, password)
-                if token:
-                    st.session_state.token = token
-                    st.session_state.username = username
-                    st.session_state.show_logged_in_message = True
-                else:
-                    st.error("Invalid User")
-
+    if st.button("Login"):
+        token = authentication(username, password)
+        if token:
+            st.session_state.token = token
+            st.session_state.username = username
+            st.session_state.show_logged_in_message = True
+        else:
+            st.error("Invalid User")
 else:
     if "show_logged_in_message" not in st.session_state:
         st.session_state.show_logged_in_message = False
 
     if st.session_state.show_logged_in_message:
         logedin_username = st.session_state.username
-        #classes = display_user_classes(st.session_state.username, st.session_state.token)
-        
 
         if logedin_username == "admin":
-            # Section to add new users
             st.header("Admin Panel - Add Users")
             new_user = st.text_input("Enter a new user:")
             new_user_password = st.text_input("Enter password for user:", type="password")
@@ -235,11 +167,7 @@ else:
                     st.success("New user added successfully!")
                 else:
                     st.error("User already exists")
-
         else:
-            # Section for Non-admin users
-
-            # Sidebar to show user Access Token
             st.sidebar.markdown("---")
             st.sidebar.markdown("<br>", unsafe_allow_html=True)
             st.sidebar.subheader("User Access Token:")
@@ -247,8 +175,6 @@ else:
             if show_token:
                 st.sidebar.code(st.session_state.token)
 
-            # Sidebar to allow user adding new model 
-            # Sidebar to allow user adding new model 
             st.sidebar.markdown("---")
             st.sidebar.markdown("<br>", unsafe_allow_html=True)
             expander = st.sidebar.expander("Add New Model")
@@ -260,36 +186,48 @@ else:
             if add_model:
                 with expander:
                     with st.spinner("Adding model..."):
-                     # Simulate some delay
                         response = add_LLM(model_name, logedin_username, HF_ACCESS_TOKEN, MAX_MODEL_LEN, SEED)
                         if response:
                             expander.success("Model added successfully!")
                         else:
                             expander.error("Error adding model")
 
-             
+            st.sidebar.markdown("---")
+            st.sidebar.markdown("<br>", unsafe_allow_html=True)
+            st.sidebar.subheader("Chat History:")
+            if st.sidebar.button("New Chat", key="new_chat"):
+                add_conversation(logedin_username, st.session_state.token)
+                user_chat_list = retrieve_user_conversations_info(logedin_username, st.session_state.token)
 
-            # Check if the user has any previous conversations, if Yes retrives the latest conversation, if not creates a new conversation
-            msgs = retrieving_messages(logedin_username,st.session_state.token)
-            if len(msgs) == 0:
-                    st.chat_message("ai").write("How can I help you?")
-            else: 
+            user_chat_list = retrieve_user_conversations_info(logedin_username, st.session_state.token)
+            if len(user_chat_list) < 0:
+                st.sidebar.write("No previous chats")
+            selected_chat = st.sidebar.radio(
+                "Select a conversation:",
+                user_chat_list["conversation_numbers"],
+                index=len(user_chat_list["conversation_numbers"]) - 1,
+            )
+
+            if "messages" not in st.session_state:
+                st.session_state.messages = []
+
+            if selected_chat:
+                st.session_state.messages = []
+                msgs = retrieving_messages(logedin_username, st.session_state.token, conversation_number=selected_chat)
                 for msg in msgs:
                     st.chat_message(msg.type).write(msg.content)
+                    st.session_state.messages.append({"role": msg.type, "content": msg.content})
 
-
-            if prompt := st.chat_input():  # (disabled=not replicate_api):
+            if prompt := st.chat_input():
                 st.session_state.messages.append({"role": "user", "content": prompt})
                 with st.chat_message("user"):
-                    if st.session_state.messages[-1]["role"] != "assistant":
-                        with st.chat_message("assistant"):
-                            with st.spinner("Thinking..."):
-                                response = chat(model="meta-llama/Llama-2-7b-chat-hf", inference_endpoint="http://localhost:8500/v1",prompt=prompt, memory=True, username=logedin_username, conversation_number='1',access_token=st.session_state.token)
-                                if response:   
-                                    response = response["data"]
-                                    placeholder = st.empty()
-                                    full_response = process_text(response)
-                                    placeholder.markdown(full_response)
-
-                            message = {"role": "assistant", "content": full_response}
-                            st.session_state.messages.append(message)
+                    st.write(prompt)
+                with st.chat_message("assistant"):
+                    with st.spinner("Thinking..."):
+                        response = chat(model="meta-llama/Llama-2-7b-chat-hf", inference_endpoint="http://localhost:8500/v1", prompt=prompt, memory=True, username=logedin_username, conversation_number=selected_chat, access_token=st.session_state.token)
+                        if response:   
+                            response = response["data"]
+                            full_response = process_text(response)
+                            st.write(full_response)
+                            st.session_state.messages.append({"role": "assistant", "content": full_response})
+                            add_conversation(logedin_username, st.session_state.messages, st.session_state.token)
